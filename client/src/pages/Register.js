fdade26577ae9b9d9493b76fcee5d62d61d816d4
Abdrahman Moussa <maloom7>
2023-11-19 23:19:56 +0300

    Routing,Login and Registeration

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Form, Input } from 'antd';
+import '../styles/RegisterStyles.css';
+import {Link} from 'react-router-dom';
 
-const RegisterPage = () => {
+const Register = () => {
+    // form handler
+    const onfinishHandler = (values) => {
+        console.log(values)
+    }
     return (
-        <div>
-           <h1>Register Page</h1>
+        <>
+        <div className="form-container">
+            <Form layout="vertical" 
+            onFinish={onfinishHandler} 
+            className="register-form">
+                <h3 className="text-center">Register From</h3>
+                <Form.Item label="Name" name="name">
+                    <Input type="text" required />
+                </Form.Item>
+                <Form.Item label="Email" name="email">
+                    <Input type="email" required />
+                </Form.Item>
+                <Form.Item label="Password" name="password">
+                    <Input type="password" required />
+                    </Form.Item>
+                    <Link to="/login" className="ms-2">Already user login here</Link>
+                    <button className="btn btn-primary" type="submit">
+                        Register
+                        </button>
+                
+
+            </Form>
+           
             </div>
+            </>
     )
 }
 
